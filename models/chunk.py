b621b515925ad48943f863d9a45df31b13f2be2f
gauthier <gauthier@intellium.ai>
2022-01-31 05:32:12 +0000

    add getter for chunk

--- models/chunk.py
+++ models/chunk.py
@@ -11,15 +11,28 @@ class Chunk(FileModel):
     def get_filename(self) -> str:
         return self.filename
 
-    def get_chunk_info(self) -> dict:
-        return {"chunk_filename": self.chunk_filename, "chunk_id": self.id}
+    def get_chunk_filename(self) -> str:
+        return self.filename
 
-    def save(self, data: bytes):
+    def save(self, data: bytes, chunks_folder: str):
         chunk_abs_path = os.path.join(
-            os.path.abspath(self.file_store.file_store_root), self.filename
+            os.path.abspath(self.file_store.file_store_root),
+            chunks_folder,
+            self.filename,
         )
         with open(chunk_abs_path, mode="wb") as fd:
             fd.write(data)
+
+    def get_data(self, chunks_folder: str) -> bytes:
+        chunk_abs_path = os.path.join(
+            os.path.abspath(self.file_store.file_store_root),
+            chunks_folder,
+            self.filename,
+        )
+        with open(chunk_abs_path, mode="rb") as fd:
+            data = fd.read()
+
+        return data
 
     def delete(self):
         os.remove(self.filename)
