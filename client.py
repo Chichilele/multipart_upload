510b997b8e060a5392ebbc985e833caacf489c6c
gauthier <gauthier@intellium.ai>
2022-02-03 10:22:42 +0000

    clean up client

--- client.py
+++ client.py
@@ -1,20 +1,21 @@
 import os
 from random import shuffle
+from shutil import ExecError
 from typing import List
 import requests
 
 
-def read_chunk(file_object, chunk_size=1024 * 100) -> bytes:
-    data = file_object.read(chunk_size)
+def read_chunk(fd, chunk_size=1024 * 100) -> bytes:
+    data = fd.read(chunk_size)
     while data:
         yield data
-        data = file_object.read(chunk_size)
+        data = fd.read(chunk_size)
 
 
-def get_chunks(content_path: str) -> dict:
+def get_chunks(fname: str, chunk_size: int) -> dict:
     chunks = {}
-    with open(content_path, "rb") as f:
-        for i, chunk in enumerate(read_chunk(f)):
+    with open(fname, "rb") as f:
+        for i, chunk in enumerate(read_chunk(f, chunk_size)):
             chunks[i] = chunk
     return chunks
 
@@ -25,30 +26,31 @@ def shuffle_dict_keys(chunks: dict) -> List[int]:
     return keys
 
 
-def main(fname, localpath, url):
-    content_path = os.path.abspath(fname)
-    print(f"Sending chunks of: {content_path}")
-    chunks = get_chunks(content_path)
+def chunk_upload(fname, dest_dir, url):
+    print(f"Sending chunks of: {fname}")
+    from file_uploader import ChunkedFileUploader
 
-    keys = shuffle_dict_keys(chunks)
+    # prepare file chunks
+    fname = "image.png"
+    chunk_size = 1024 * 100
+    chunks = get_chunks(fname, chunk_size)
+    nb_chunks = len(chunks)
 
-    chunk_size: int = 1024 * 100
-    chunked_content_path: str = content_path + ".chunked.png"
-    for chunk_id in keys:
-        with open(chunked_content_path, "wb") as f:
-            chunk = chunks[chunk_id]
-            f.seek(chunk_size * chunk_id)
-            f.write(chunk)
+    ## 1. Initiate file upload
+    uploader = ChunkedFileUploader()
+    uploader.start(filename=fname, nb_chunks=nb_chunks, chunk_size=chunk_size)
 
-    print(f"Sending chunks of: {chunked_content_path}")
-    return
-    for i in keys:
-        r = requests.post(url, data=chunks[i], headers={"dxchunkid": str(i)})
-        print(f"chunk {i:3}\tr: {r}")
+    ## 2. Upload chunks in random order
+    shuffled_keys = shuffle_dict_keys(chunks)
+    for i in shuffled_keys:
+        uploader.upload_chunk(fname, i, 1024 * 100, chunks[i])
+
+    ## 3. Finalise the upload
+    uploader.finalise(fname)
 
 
 if __name__ == "__main__":
-    filename = "image.png"  # ~1GB
-    localpath = "uploads"
-    url = "http://localhost:8080/upload/{0}".format(filename)
-    main(filename, url)
+    fname = "image.png"  # ~1GB
+    dest_dir = "uploads"
+    url = "http://localhost:8080/upload/{0}".format(fname)
+    chunk_upload(fname, dest_dir, url)
